3521a3c6d7445ec9b79a99e5368288e7309b2b70
noahlunot <noahlunot0000@gmail.com>
2023-09-06 02:01:28 +0800

    first commit

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//Make sure to change the metadata.set("authorization", "Key YOUR_API_KEY"); to your own api key
+
 //express
 const express = require('express')
 const app = express()
@@ -21,6 +23,41 @@ const db = knex({
     }
 });
 
+app.post('/imageurl', (req, res) => {
+    const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+    const stub = ClarifaiStub.grpc();
+    
+    const metadata = new grpc.Metadata();
+    metadata.set("authorization", "Key YOUR_API_KEY");
+
+    stub.PostModelOutputs(
+        {
+            // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+            model_id: "face-detection",
+            inputs: [{data: {image: {url: req.body.input}}}]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            }
+    
+            if (response.status.code !== 10000) {
+                console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+                return;
+            }
+    
+            console.log("Predicted concepts, with confidence values:")
+            for (const c of response.outputs[0].data.concepts) {
+                console.log(c.name + ": " + c.value);
+            }
+        res.json(response)
+        }
+    );
+})
+
 app.get('/', (req, res) => {
     res.json(database)
 })
@@ -39,6 +76,9 @@ app.get('/profile/:id', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
+    if(!req.body.email || !req.body.password) {
+        return res.status(400).json("invalid information")
+    }
     db.select('email', 'hash').where('email', '=', req.body.email).from('signin').then(user => {
         bcrypt.compare(req.body.password, user[0].hash, function(err, hash) {
             if(hash) {
@@ -50,6 +90,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/signup', (req, res) => {
     const {name, email, password} = req.body;
+    if(!email || !name || !password) {
+        return res.status(400).json("invalid information")
+    }
     bcrypt.hash(password, saltRounds, function(err, hash) {
         db.transaction(trx => {
             trx.insert({
